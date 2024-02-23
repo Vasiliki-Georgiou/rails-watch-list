fae87f5e827587ef714ec28db61a7421a894a3a9
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-23 11:00:28 +0100

    Adding some corrections to bookmarks and lists controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,7 +9,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
-    redirect_to lists_path(@list)
+    redirect_to lists_path
   end
 
   private

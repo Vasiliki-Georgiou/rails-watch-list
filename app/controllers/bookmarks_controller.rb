1a521d76948c552d101b0d85e9ae78d83ab106c6
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-23 12:35:44 +0100

    Adding some corrections along with delete actions for list and bookmarks, added delete buttons

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,12 @@ class BookmarksController < ApplicationController
     redirect_to lists_path
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path(@list)
+  end
+
   private
 
   def bookmark_params
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :lists, only: [:index, :new, :create, :show] do
+  root "lists#index"
+  resources :lists, only: [:index, :new, :create, :show, :destroy] do
 
     resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
   end
